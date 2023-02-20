a9d798732cc519840360434234134080e5c625f4
Victoria Moor <victimofuroboros@gmail.com>
2023-02-20 11:45:56 +0500

    task1_corrected

--- sem1/task1.py
+++ sem1/task1.py
@@ -4,4 +4,4 @@ a = int(input('Enter a 3-digit number: '))
 if a < 100 or a > 999:
     print('Please, enter correct number.')
 else:
-    print(f'The sum of digits is: {a % 10 + (a // 10) % 10 + a // 100}')
+    print(f'The sum of digits is: {a % 10 + a // 10 % 10 + a // 100}')

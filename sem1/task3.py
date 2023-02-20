422742477c31a490e49b1ca0eccc58e8a5129d4f
Victoria Moor <victimofuroboros@gmail.com>
2023-02-20 11:51:56 +0500

    task3_corrected

--- sem1/task3.py
+++ sem1/task3.py
@@ -5,8 +5,7 @@ a = tick // 100000 + (tick // 10000) % 10 + (tick // 1000) % 10
 b = (tick // 100 % 10) + (tick // 10 % 10) + tick % 10
 if tick < 100000 or tick > 999999:
     print("Enter 6-digit number next time, please.")
+elif a == b:
+    print('yes')
 else:
-    if a == b:
-        print('yes')
-    else:
-        print('no')
+    print('no')

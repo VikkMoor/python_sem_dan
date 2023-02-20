f8244b69d7ecf8bdc8a65abdf6c8768679e6fbb3
Victoria Moor <victimofuroboros@gmail.com>
2023-02-20 12:30:26 +0500

    Update task2.py

--- sem1/task2.py
+++ sem1/task2.py
@@ -1,7 +1,7 @@
 """Input - total number of origami (s), output - how much each of 3 kids made (Sergio = Peter, Kate = 2*(Sergio+Peter))"""
 
 s = int(input('Enter total number (multiple of 4): '))
-if s % 4 == 0:
-    print(f'Peter made: {s // 4}, Kate made: {s // 2}, Sergio made: {s // 4}.')
+if s % 6 == 0:
+    print(f'Peter made: {s // 6}, Kate made: {2 * s // 3}, Sergio made: {s // 6}.')
 else:
     print("Kids can't made this number of origami cuz of the task.")

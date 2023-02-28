501535574658cc9d2ac11e0ec0264986472acc74
Vikk_Moor <victimofuroboros@gmail.com>
2023-03-01 04:48:07 +0500

    task3_ready

---
+++ sem2/task3.py
@@ -0,0 +1,16 @@
+"""Task bout "scrabble" game and counting the price of english/russian word"""
+points = {1: 'АВЕИНОРСТAEIOULNSTR',
+          2: 'ДКЛМПУDG',
+          3: 'БГЁЬЯBCMP',
+          4: 'ЙЫFHVWY',
+          5: 'ЖЗХЦЧK',
+          8: 'ШЭЮJZ',
+          10: 'ФЩЪQZ'}
+word = input('Enter your word: ').upper()
+price = 0
+for i in word:
+    if i in 'QWERTYUIOPASDFGHJKLZXCVBNMЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ':
+        for j in points:
+            if i in points[j]:
+                price += j
+print(price)
